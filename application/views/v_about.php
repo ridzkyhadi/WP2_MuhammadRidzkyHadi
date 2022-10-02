2c21ca0c4868d617530c4f512c2a8a01bb37dcac
ridzkyhadi <ridzky777@gmail.com>
2022-10-02 23:02:48 +0700

    menambahkan .

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Muhammad Ridzky</li> 
+        <li>Nama Belakang : Hadi</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Beringin II Anjungan</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Putussibau</li> 
+    </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+    </ul> 
+</section>
